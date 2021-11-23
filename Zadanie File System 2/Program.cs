759c82eb3e33872d10f77398a1a9d33e18a142d3
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-11-23 18:02:57 +0300

    Add files via upload

---
+++ Zadanie File System 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Zadanie_File_System_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Nikryuchkov\Desktop\New folder (2)\New Text Document.txt";
+            
+            int[] massiv = new int[10];
+            Random random = new Random();
+            StreamWriter sr = new StreamWriter(path);
+            foreach (int i in massiv)
+            {
+                massiv[i] = random.Next();
+                sr.WriteLine(massiv[i]);
+               
+            }
+            sr.Close();
+            StreamReader st = new StreamReader(path);
+            StreamReader sk = new StreamReader(path);
+            Console.Write(((st.ReadToEnd())));
+            int[] massiv_2 = new int[10];
+            int sm=0;
+            foreach (int k in massiv_2)
+            {
+                massiv_2[k] =Convert.ToInt32( sk.ReadLine());
+                sm = sm+ massiv_2[k];
+                Console.WriteLine(massiv_2[k]);
+            }
+            Console.WriteLine("Сумма чисел в последовательности равна {0}", sm);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Zadanie File System/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+namespace Zadanie_File_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Nikryuchkov\Desktop\6.3.2.ЭДП";
+            if(Directory.Exists(path))
+                {
+                    foreach (string i in Directory.GetDirectories(path) )
+                {
+                    Console.WriteLine(i);
+                    foreach (string k in Directory.GetDirectories(i))
+                    {
+                        Console.WriteLine(k);
+                    }
+                    foreach (string l in Directory.GetFiles(i))
+                    {
+                        Console.WriteLine(l);
+                    }
+                }
+                    foreach (string j in Directory.GetFiles(path))
+                {
+                    Console.WriteLine(j);
+                }
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
